652f3762d08261376f651a32119761f1c306aceb
JiiXaa <anythingfx@gmail.com>
2022-03-31 20:57:01 +0100

    login form and Alert component

---
+++ client/src/components/Alert.js
@@ -0,0 +1,5 @@
+const Alert = () => {
+  return <div className='alert alert-danger'>alert will be here</div>;
+};
+
+export default Alert;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,7 +1,63 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import { Logo, FormRow, Alert } from '../components';
+import StyledWrapper from '../assets/styledWrappers/RegisterPage';
+
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
+  showAlert: false,
+};
 
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+  // global state and useNavigate
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
+  return (
+    <StyledWrapper className='full-page'>
+      <form className='form' onSubmit={handleSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* Show Alert */}
+        {values.showAlert && <Alert />}
+        {/* name input */}
+        <FormRow
+          type='text'
+          name='name'
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email input */}
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password input */}
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
+
+        <button type='submit' className='btn btn-block'>
+          submit
+        </button>
+      </form>
+    </StyledWrapper>
+  );
 };
 
 export default Register;
