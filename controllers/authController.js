b194e573743504f3afead24df8f8b385a8c8cf7e
JiiXaa <anythingfx@gmail.com>
2022-07-06 20:12:14 +0100

    login controller / password comparison

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 import User from '../models/User.js';
 import { StatusCodes } from 'http-status-codes';
-import { BadRequestError } from '../errors/index.js';
+import { BadRequestError, UnAuthenticatedError } from '../errors/index.js';
 
 // to make code cleaner I use express-async-errors package, it does try/catch behind scenes
 // express-async-errors imported in server.js
@@ -44,7 +44,26 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-  res.send('login user');
+  const { email, password } = req.body;
+  if (!email || !password) {
+    throw new BadRequestError('Please provide all values!');
+  }
+  // We need to add select method which adds password to the findOne user object. That is because in User model password has select set to false and does not exist in this case as we changed this in user creation phase, because we wanted to hide it at that point. Now we need it for bcrypt to make comparison.
+  const user = await User.findOne({ email }).select('+password');
+  if (!user) {
+    throw new UnAuthenticatedError('Invalid Credentials!');
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
+    throw new UnAuthenticatedError('Invalid Credentials!');
+  }
+  const token = user.createJWT();
+
+  // this is how we can "hide" password as we don't need it any more and it is not good practice to send sensitive data.
+  user.password = undefined;
+
+  res.status(StatusCodes.OK).json({ user, token, location: user.location });
 };
 
 const updateUser = async (req, res) => {
