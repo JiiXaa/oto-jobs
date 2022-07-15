cb8801038b76907670d0cc240f47574f24104715
JiiXaa <anythingfx@gmail.com>
2022-07-15 19:56:19 +0100

    small screen sidebar structure

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,9 +1,24 @@
 import Wrapper from '../assets/styledWrappers/SmallSidebar';
+import { FaTimes } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
 
 const SmallSidebar = () => {
   return (
     <Wrapper>
-      <h4>SmallSidebar</h4>
+      <div className='sidebar-container show-sidebar'>
+        <div className='content'>
+          <button className='close-btn' onClick={() => console.log('toggle')}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>nav links</div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
