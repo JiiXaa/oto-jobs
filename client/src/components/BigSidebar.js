0f040a50367bd24b5c30584ba5ca591b4bb51be0
JiiXaa <anythingfx@gmail.com>
2022-07-18 20:17:51 +0100

    added NavLinks component to the bigSidebar and CSS

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,9 +1,26 @@
+import { useAppContext } from '../context/appContext';
+import NavLinks from './NavLinks';
+import Logo from './Logo';
 import Wrapper from '../assets/styledWrappers/BigSidebar';
 
 const BigSidebar = () => {
+  const { showSidebar } = useAppContext();
+
   return (
     <Wrapper>
-      <h4>BigSidebar</h4>
+      <div
+        className={
+          showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+          {/* You can load toggleSidebar from useAppContext() and add it to props to the NavLinks component, it will close navbar after clicking one, same behavior as smallSidebar has if you like. */}
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
