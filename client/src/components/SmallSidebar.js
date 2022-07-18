338666efc09b1d087fae9bcbef03091f74c06f3b
JiiXaa <anythingfx@gmail.com>
2022-07-18 20:05:03 +0100

    added NavLinks as a seperate component for easy reusability for small and big sidebar

--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,15 +1,18 @@
 import Wrapper from '../assets/styledWrappers/SmallSidebar';
 import { FaTimes } from 'react-icons/fa';
 import { useAppContext } from '../context/appContext';
-import links from '../utils/links';
-import { NavLink } from 'react-router-dom';
 import Logo from './Logo';
+import NavLinks from './NavLinks';
 
 const SmallSidebar = () => {
-  const {showSidebar, toggleSidebar, isLoading} = useAppContext()
+  const { showSidebar, toggleSidebar } = useAppContext();
   return (
     <Wrapper>
-      <div className={showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'}>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
         <div className='content'>
           <button className='close-btn' onClick={toggleSidebar}>
             <FaTimes />
@@ -17,18 +20,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className='nav-links'>
-            {links.map((link) => {
-              const {text, path, id, icon} = link
-
-              return (
-                <NavLink to={path} key={id} onClick={toggleSidebar} className={({isActive}) => isActive ? 'nav-link active' : 'nav-link'}>
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              )
-            })}
-          </div>
+          <NavLinks toggleSidebar={toggleSidebar} />
         </div>
       </div>
     </Wrapper>
