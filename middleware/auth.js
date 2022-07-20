74da9b155e3d22f9d661895116e849a26b17a58a
JiiXaa <anythingfx@gmail.com>
2022-07-20 21:28:19 +0100

    unauthenticated error when header jwt bearer missing

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,15 @@
-const auth = async (req,res,next) => {
-  console.log('authenticate user middleware');
-  next()
-}
+import { UnAuthenticatedError } from '../errors/index.js';
 
-export default auth
+const auth = async (req, res, next) => {
+  // const headers = req.headers;
+  // console.log(headers);
+  //// authHeader its authorization bearer jwt token and if missing we throw error
+  const authHeader = req.headers.authorization;
+  // console.log(authHeader);
+  if (!authHeader) {
+    throw new UnAuthenticatedError('Authentication Invalid');
+  }
+  next();
+};
+
+export default auth;
