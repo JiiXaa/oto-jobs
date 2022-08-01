edbfa333b0438a226f93a4eb1ba147364e2fccba
JiiXaa <anythingfx@gmail.com>
2022-08-01 21:14:06 +0100

    Form Row Select component for handling multiple selects DRY

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,8 +1,17 @@
 import Logo from './Logo';
 import FormRow from './FormRow';
+import FormRowSelect from './FormRowSelect';
 import Alert from './Alert';
 import Navbar from './Navbar';
 import BigSidebar from './BigSidebar';
 import SmallSidebar from './SmallSidebar';
 
-export { Logo, FormRow, Alert, Navbar, BigSidebar, SmallSidebar };
+export {
+  Logo,
+  FormRow,
+  Alert,
+  Navbar,
+  BigSidebar,
+  SmallSidebar,
+  FormRowSelect,
+};
--- client/src/pages/dashboard/AddJob.js
+++ client/src/pages/dashboard/AddJob.js
@@ -1,4 +1,4 @@
-import { FormRow, Alert } from '../../components';
+import { FormRow, FormRowSelect, Alert } from '../../components';
 import { useAppContext } from '../../context/appContext';
 import Wrapper from '../../assets/styledWrappers/DashboardFormPage';
 
@@ -59,28 +59,22 @@ const AddJob = () => {
             value={jobLocation}
             handleChange={handleJobInput}
           />
-          {/* job type */}
-          <div className='form-row'>
-            <label htmlFor='jobType' className='form-label'>
-              job type
-            </label>
-            <select
-              name='jobType'
-              id='jobType'
-              value={jobType}
-              onChange={handleJobInput}
-              className='form-select'
-            >
-              {jobTypeOptions.map((itemValue, index) => {
-                return (
-                  <option key={index} value={itemValue}>
-                    {itemValue}
-                  </option>
-                );
-              })}
-            </select>
-          </div>
           {/* job status */}
+          <FormRowSelect
+            name='status'
+            value={status}
+            handleChange={handleJobInput}
+            list={statusOptions}
+          />
+          {/* job type */}
+          <FormRowSelect
+            name='jobType'
+            labelText='job type'
+            value={jobType}
+            handleChange={handleJobInput}
+            list={jobTypeOptions}
+          />
+          {/* btn container */}
           <div className='btn-container'>
             <button
               type='submit'
