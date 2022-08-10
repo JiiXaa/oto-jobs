2f00c1269435cb482d51a68f2583f95a53794f51
JiiXaa <anythingfx@gmail.com>
2022-08-10 21:01:41 +0100

    frontend: JobType component

--- client/src/components/JobInfo.js
+++ client/src/components/JobInfo.js
@@ -1,5 +1,12 @@
-const JobInfo = () => {
-  return <h2>Job Info</h2>;
+import Wrapper from '../assets/styledWrappers/JobInfo';
+
+const JobInfo = ({ icon, text }) => {
+  return (
+    <Wrapper>
+      <span className='icon'>{icon}</span>
+      <span className='text'>{text}</span>
+    </Wrapper>
+  );
 };
 
 export default JobInfo;
